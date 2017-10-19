043eaa9cfd40ab249c62a9b9986d3f38d4b0995c
henry2992 <henry2992@hotmail.com>
2017-10-19 17:50:08 -0500

    WIP

--- app/uploaders/pet_uploader.rb
+++ app/uploaders/pet_uploader.rb
@@ -1,8 +1,18 @@
 class PetUploader < CarrierWave::Uploader::Base
 
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
   include Cloudinary::CarrierWave
 
-  def public_id
+ # Choose what kind of storage to use for this uploader:
+  # storage :file
+  # storage :fog
+
+ # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 end
