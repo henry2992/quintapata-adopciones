b95b20406f93be0e7a02550f465da9d797e85b32
mglriosr <mgl.rios.r@gmail.com>
2017-10-23 17:35:46 -0500

    Update koala.rb

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,6 +1,6 @@
 Koala.configure do |config|
-  config.access_token = "EAAEqUFsbo8ABAFoScoWevEtcf6tDFlMqGAcS7hBYDTjAh2umk2FCPdEYLQ3Aro29P5XJdUecWTh1oyMNWiDYioou5OwHH4oOzesJhaTN9kcHBvaArI47zl5h0OBly7zeQSYDh8ZBfrjDrZB2iSeNFSJ7x8FQoB3WTdJQMdNG9mZBChrMvOZBPHDSGTYUyXcZD"
-  config.app_access_token = "327999590998976|qKz05PWxySP2bxd2-UfJ8exNPHs"
+  config.access_token = "EAAHRYsySSjgBAOYXAyYnGobDHo7fSu4zwWuRYpI739qMFEZAGOUySWfm78fC5zddmhZBSddwZBE3TxIdc2ZClnSxKcYMNMBDX3yPrW16SgstzsM2YOkW8ZBvn7KTZA0lZBKCbp3us57h9s6rcidUOMpSijO0E9N58EzX0r8ZBoZBl8CZB5Wlw6x8IDXh8OZCdgl0E4ZD"
+  config.app_access_token = "511697245850168|Cab3GaKoyqugrAYKNISH7ee2UWE"
   config.app_id = "327999590998976"
   config.app_secret = "01b43792ba39331fb4660a2b40543323"
 
